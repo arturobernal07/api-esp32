c2cb20c8953fc9199a51f3686fff10eb4ea1a527
arturobernal07 <bernalartur363@gmail.com>
2026-03-23 16:30:53 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pymongo import MongoClient
+from datetime import datetime
+
+app = FastAPI()
+
+MONGO_URI = "mongodb+srv://esp32:esp32password@cluster0.p6mmu.mongodb.net/iot?retryWrites=true&w=majority"
+client = MongoClient(MONGO_URI)
+db = client.iot
+collection = db.sensores
+
+@app.get("/") 
+def root():
+    return {"mensaje":"API funcionando"}
+
+@app.post("/sensor")
+def guardar_sensor(data: dict):
+    data["fecha"] = datetime.now()
+    collection.insert_one(data) 
+    return {"status":"dato guardado"}
